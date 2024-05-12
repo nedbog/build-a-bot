f3d4e9e5a60490e7e08d1cc131e057f8458be9d4
Bogdan Nedelcu <nedbog@gmail.com>
2024-05-12 18:05:04 +0300

    module 2

---
+++ src/components/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" aria-hidden="true" alt="Robot">
+    </div>
+    <div class="get-started">
+      <a href="">Get started</a> building your firts robot
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
